2ee931ef6b3fce056662e4420e895ed6f624721f
GreenyDev <greenydev@greenmail.net.au>
2014-10-22 09:11:25 +1030

    Added a nice little header in the Preferences Pane
    And fixed com.apple.springboard

--- centerstageprefs/centerstageprefs.mm
+++ centerstageprefs/centerstageprefs.mm
@@ -1,4 +1,15 @@
 #import <Preferences/Preferences.h>
+#import <Preferences/PSTableCell.h>
+
+int width = [[UIScreen mainScreen] bounds].size.width;
+
+@protocol PreferencesTableCustomView
+- (id)initWithSpecifier:(id)arg1;
+
+@optional
+- (CGFloat)preferredHeightForWidth:(CGFloat)arg1;
+- (CGFloat)preferredHeightForWidth:(CGFloat)arg1 inTableView:(id)arg2;
+@end
 
 @interface centerstageprefsListController: PSListController {
 }
@@ -17,5 +28,40 @@
 }
 - (void)donate{
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=9ZXVHGA5AW5CG&lc=AU&item_name=GreenyDev&currency_code=AUD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted"]];
+}
+@end
+
+@interface DataManagerCustomCell : PSTableCell <PreferencesTableCustomView> {
+	UILabel *_label;
+}
+@end
+
+@interface PSTableCell (CenterStage)
+- (id)initWithStyle:(int)style reuseIdentifier:(id)arg2;
+@end
+
+@implementation DataManagerCustomCell
+- (id)initWithSpecifier:(PSSpecifier *)specifier
+{
+	self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+	if (self) {
+		CGRect frame = CGRectMake(0, -5, width, 60);
+ 
+		_label = [[UILabel alloc] initWithFrame:frame];
+		[_label setNumberOfLines:1];
+		_label.font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:48];
+		[_label setText:@"CenterStage"];
+		[_label setBackgroundColor:[UIColor clearColor]];
+		_label.textColor = [UIColor blackColor];
+		_label.textAlignment = NSTextAlignmentCenter;
+ 
+		[self addSubview:_label];
+	}
+	return self;
+}
+ 
+- (CGFloat)preferredHeightForWidth:(CGFloat)arg1 {
+	CGFloat prefHeight = 90.0;
+	return prefHeight;
 }
 @end
